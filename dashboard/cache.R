a8deeb98032afcf139bcbf61869ee9b27a4c19e3
rqthomas <rqthomas@vt.edu>
2023-05-24 23:48:44 +0000

    trying to get dashboard working

--- dashboard/cache.R
+++ dashboard/cache.R
@@ -1,10 +1,10 @@
 
-# remotes::install_github("cboettig/minio")
+remotes::install_github("cboettig/minio")
 library(minio)
 install_mc()
 mc_alias_set("efi",  endpoint="data.ecoforecast.org",
              access_key = "", secret_key = "")
 
-mc("mirror --overwrite efi/neon4cast-scores cache/")
+mc("mirror --overwrite efi/tern4cast-scores cache/")
 
 
